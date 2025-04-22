27f0a4c747769724e92ecfcb209c94955713b18d
literally-bug-creator <literallybugcreator@gmail.com>
2025-04-22 12:16:58 +0300

    Change coefficient interface, implement coefficient.cpp

--- include/coefficient.h
+++ include/coefficient.h
@@ -1,12 +1,18 @@
 #pragma once
 
+#include <iterator>
+#include <string>
+
+const std::string EMPTY_ERROR_MSG = "";
+
 struct Coefficient{
-    const float value;
+    const double value;
+    const std::string error_msg;
+
+    Coefficient(const double number, const std::string &error_msg = EMPTY_ERROR_MSG) : value(number), error_msg(error_msg) {}
 };
 
 Coefficient get_invalid_coefficient();
-
-Coefficient get_coefficient();
 
 bool is_valid(const Coefficient &coeff);
 
@@ -19,3 +25,5 @@ Coefficient mul(const Coefficient &multiplicand, const Coefficient &multiplier);
 Coefficient div(const Coefficient &dividend, const Coefficient &divisor);
 
 Coefficient square_root(const Coefficient &radicand);
+
+Coefficient get_coefficient();
---
+++ src/coefficient.cpp
@@ -0,0 +1,50 @@
+#include "../include/coefficient.h"
+#include <cmath>
+#include <string>
+
+const double DEFAULT_VALUE = 0.0;
+const double ZERO = 0.0;
+
+const std::string DIV_BY_ZERO_MSG = "Низя делить на нуль!";
+const std::string ROOT_FROM_NEG_VALUE_MSG = "Низя брать корень из отрицательного числа!";
+
+Coefficient get_invalid_coefficient(){
+    return Coefficient(DEFAULT_VALUE);
+}
+
+bool is_valid(const Coefficient &coeff){
+    return coeff.error_msg != EMPTY_ERROR_MSG;
+}
+
+Coefficient add(const Coefficient &augend, const Coefficient &addend){
+    double sum = augend.value + addend.value;
+    return Coefficient(sum);
+}
+
+Coefficient sub(const Coefficient &minuend, const Coefficient &subtrahend){
+    double difference = minuend.value - subtrahend.value;
+    return Coefficient(difference);
+}
+
+Coefficient mul(const Coefficient &multiplicand, const Coefficient &multiplier){
+    double product = multiplicand.value * multiplier.value;
+    return Coefficient(product);
+}
+
+Coefficient div(const Coefficient &dividend, const Coefficient &divisor){
+    if (divisor.value == ZERO){
+        return Coefficient(DEFAULT_VALUE, DIV_BY_ZERO_MSG);
+    }
+
+    double quotient = dividend.value / divisor.value;
+    return Coefficient(quotient);
+}
+
+Coefficient square_root(const Coefficient &radicand){
+    if (radicand.value < ZERO){
+        return Coefficient(DEFAULT_VALUE, ROOT_FROM_NEG_VALUE_MSG);
+    }
+
+    double root = sqrt(radicand.value);
+    return Coefficient(root);
+}
