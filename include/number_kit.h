7282761c34140ce3fd4ad452d0c88b90a66c7346
literally-bug-creator <literallybugcreator@gmail.com>
2025-04-29 13:51:16 +0300

    Add interface - "NumberKit" w/ impl.

---
+++ include/number_kit.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "number.h"
+
+struct NumberKit {
+    const Number a;
+    const Number b;
+    const Number c;
+    const Error error;
+
+    NumberKit( const Number& a,
+               const Number& b,
+               const Number& c,
+               const Error& error ):
+        a( a ), b( b ), c( c ), error( error ) {}
+};
+
+NumberKit make_number_kit( const Number& a,
+                           const Number& b,
+                           const Number& c,
+                           const Error& error = DEFAULT_ERROR );
+
+NumberKit input_number_kit();
+
+Number get_a( const NumberKit& nums );
+
+Number get_b( const NumberKit& nums );
+
+Number get_c( const NumberKit& nums );
+
+Error get_error( const NumberKit& nums );
---
+++ src/number_kit.cpp
@@ -0,0 +1,49 @@
+#include "../include/number_kit.h"
+
+namespace {
+    const std::string INVALID_INPUT_MSG = "Некорректный ввод";
+
+    const Error INVALID_INPUT_ERROR =
+        make_error( INVALID_INPUT, INVALID_INPUT_MSG );
+
+    bool has_error( const Number& number ) {
+        Error error = get_error( number );
+
+        return error.code != OK;
+    }
+
+    bool has_error( const Number& a, const Number& b, const Number& c ) {
+        return has_error( a ) || has_error( b ) || has_error( c );
+    }
+
+    bool has_null( const Number& a, const Number& b, const Number& c ) {
+        return is_null( a ) || is_null( b ) || is_null( c );
+    }
+}
+
+NumberKit make_number_kit( const Number& a,
+                           const Number& b,
+                           const Number& c,
+                           const Error& error ) {
+    return NumberKit( a, b, c, error );
+}
+
+NumberKit input_number_kit() {
+    Number a = input_number();
+    Number b = input_number();
+    Number c = input_number();
+
+    if ( has_error( a, b, c ) || has_null( a, b, c ) ) {
+        return make_number_kit( a, b, c, INVALID_INPUT_ERROR );
+    }
+
+    return make_number_kit( a, b, c );
+}
+
+Number get_a( const NumberKit& nums ) { return nums.a; }
+
+Number get_b( const NumberKit& nums ) { return nums.b; }
+
+Number get_c( const NumberKit& nums ) { return nums.c; }
+
+Error get_error( const NumberKit& nums ) { return nums.error; }
