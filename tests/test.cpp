c0b3132f35d25ec07ada656be42cb253f0cd4e17
literally-bug-creator <literallybugcreator@gmail.com>
2025-04-29 13:09:27 +0300

    Add blank test file

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
+#include "../include/coefficient.h"
 
 TEST_CASE( "Simple test", "[example]" ) { REQUIRE( 1 + 1 == 2 ); }
 
-TEST_CASE( "Math test #1" ) { REQUIRE( 1 + 1 == 3 ); }
-
-TEST_CASE( "Math test #2" ) { REQUIRE( 1 + 1 == 4 ); }
-
-TEST_CASE( "Math test #3" ) { REQUIRE( 1 + 1 == 5 ); }
+TEST_CASE( "Simple test", "[example]" ) {
+    Coefficient a = Coefficient(0.0);
+    Coefficient b = Coefficient(0.0);
+    REQUIRE( is_equal(add(a, b), b) );
+}
