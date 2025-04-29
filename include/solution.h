684895f3e65bbd15c59c1dc858be86dda06d312f
literally-bug-creator <literallybugcreator@gmail.com>
2025-04-29 15:02:41 +0300

    Update "Solution" interface

--- include/solution.h
+++ include/solution.h
@@ -1,38 +1,40 @@
 #pragma once
 
-#include <string>
-
-#include "coefficient.h"
-#include "coefficient_set.h"
+#include "number.h"
+#include "number_kit.h"
 
 enum SolutionType {
-    QUADRATIC,
-    LINEAR,
+    TWO_ROOTS,
+    ONE_ROOT,
     NO_ROOTS,
     ANY_ROOT,
+    NO_SOLUTION,
 };
 
 struct Solution {
     const SolutionType type;
-    const Coefficient x1;
-    const Coefficient x2;
-    const std::string error_msg;
+    const Number x1;
+    const Number x2;
+    const Error error;
 
     Solution( const SolutionType& type,
-              const Coefficient& x1,
-              const Coefficient& x2,
-              const std::string& error_msg = EMPTY_ERROR_MSG ):
-        type( type ), x1( x1 ), x2( x2 ), error_msg( error_msg ) {}
+              const Number& x1,
+              const Number& x2,
+              const Error& error ):
+        type( type ), x1( x1 ), x2( x2 ), error( error ) {}
 };
+
+Solution make_solution( const SolutionType& type,
+                        const Number& first_root,
+                        const Number& second_root,
+                        const Error& error = DEFAULT_ERROR );
+
+Solution get_solution( const NumberKit& nums );
 
 SolutionType get_solution_type( const Solution& solution );
 
-Coefficient get_first_root( const Solution& solution );
+Number get_first_root( const Solution& solution );
 
-Coefficient get_second_root( const Solution& solution );
+Number get_second_root( const Solution& solution );
 
-std::string get_error_msg( const Solution& solution );
-
-bool is_valid( const Solution& solution );
-
-Solution get_solution( const CoefficientSet& coeffs );
+Error get_error( const Solution& solution );
