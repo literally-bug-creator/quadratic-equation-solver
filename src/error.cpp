549b6d8b56f7cf4998767baa1af0ac8933251792
literally-bug-creator <literallybugcreator@gmail.com>
2025-04-30 12:35:17 +0300

    Implement interface func

--- src/error.cpp
+++ src/error.cpp
@@ -1,5 +1,9 @@
 #include "../include/error.h"
 
+Error make_error( const ErrorCode code, const std::string& message ) {
+    return Error( code, message );
+}
+
 ErrorCode get_error_code( const Error& error ) { return error.code; }
 
 std::string get_error_message( const Error& error ) { return error.message; }
