e82fd359ed09db7d61cc1306f20d8535721c213e
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-01 18:45:57 +0300

    Correct main.py

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
+
 #include "../include/number_kit.h"
 #include "../include/output.h"
 #include "../include/solution.h"
 
 int main() {
-    NumberKit nums = input_number_kit();
+    NumberKit nums = input_number_kit( std::cin );
     Solution solution = get_solution( nums );
     output( solution );
 }
