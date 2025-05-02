6c0c75d32b3a6d5f3f015b63ce47a70780ea4524
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-02 15:50:32 +0300

    Update tools

--- tests/tools.cpp
+++ tests/tools.cpp
@@ -4,7 +4,7 @@
 #include "../include/number.h"
 #include "../include/number_kit.h"
 #include "../include/solution.h"
-
+#include <iostream>
 
 const int DEFAULT_MIN_INT = 0;
 const int DEFAULT_MIN_NEG_INT = -10000;
@@ -59,31 +59,79 @@ inline Error random_error() {
     return make_error( code, msg );
 }
 
-inline Number random_number(){
-    double value = random_double(DEFAULT_MIN_NEG_INT, DEFAULT_MAX_INT);
-    return make_number(value);
+inline Number random_number() {
+    double value = random_double( DEFAULT_MIN_NEG_INT, DEFAULT_MAX_INT );
+    return make_number( value );
 }
 
-inline NumberKit random_number_kit(){
+inline Number random_number( int min, int max ) {
+    double value = random_double( min, max );
+    return make_number( value );
+}
+
+inline NumberKit random_number_kit() {
     Number a = random_number();
     Number b = random_number();
     Number c = random_number();
 
-    return make_number_kit(a, b, c);
+    return make_number_kit( a, b, c );
 }
 
-inline SolutionType random_solution_type(){
+inline SolutionType random_solution_type() {
     static std::random_device rd;
     static std::mt19937 gen( rd() );
     std::uniform_int_distribution<int> dist( 0, SOLUTION_TYPES_AMOUNT - 1 );
     return static_cast<SolutionType>( dist( gen ) );
 }
 
-inline Solution random_solution(){
+inline Solution random_solution() {
     SolutionType type = random_solution_type();
     Number x1 = random_number();
     Number x2 = random_number();
     Error error = random_error();
 
-    return make_solution(type, x1, x2, error);
+    return make_solution( type, x1, x2, error );
+}
+
+inline Solution random_two_roots_solution() {
+    Number a = random_number();
+    Number c = random_number();
+    double min_b = mul( mul( make_number( 4 ), a ), c ).value;
+    double max_b = std::abs(min_b) * 2;
+    Number b = random_number( min_b, max_b );
+    NumberKit kit = make_number_kit( a, b, c );
+
+    return get_solution( kit );
+}
+
+inline Solution random_single_root_solution() {
+    Number a = random_number();
+    Number c = random_number();
+    double b_value = sqrt( mul( mul( make_number( 4 ), a ), c ).value );
+    Number b = make_number( b_value );
+    NumberKit kit = make_number_kit( a, b, c );
+
+    return get_solution( kit );
+}
+
+inline Solution random_linear_solution() {
+    Number a = make_number( 0 );
+    Number b = random_number();
+    Number c =
+        make_number( random_double( DEFAULT_MIN_INT, DEFAULT_MAX_INT ) + 1 );
+    NumberKit kit = make_number_kit( a, b, c );
+
+    return get_solution( kit );
+}
+
+inline Solution random_no_roots_solution() {
+    Number a = random_number();
+    Number c = random_number();
+    double max_b = mul( mul( make_number( 4 ), a ), c ).value - 1;
+    double min_b =
+        max_b - random_double( DEFAULT_MIN_INT, DEFAULT_MAX_INT );
+    Number b = random_number( min_b, max_b );
+    NumberKit kit = make_number_kit( a, b, c );
+
+    return get_solution( kit );
 }
