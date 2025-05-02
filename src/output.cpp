da4215b41e73415e67557db387a4ba308c04b95f
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-02 15:50:15 +0300

    Update `output` impl.

--- src/output.cpp
+++ src/output.cpp
@@ -1,6 +1,6 @@
 #include "../include/output.h"
 
-#include <iostream>
+#include <ostream>
 
 const std::string FIRST_ROOT_MSG = "X1 = ";
 const std::string SECOND_ROOT_MSG = "X2 = ";
@@ -14,68 +14,65 @@ namespace {
         return get_error_code( error ) != OK;
     }
 
-    void output_error_solution( const Solution& solution ) {
+    void output_error_solution( std::ostream& destination,
+                                const Solution& solution ) {
         Error error = get_error( solution );
         std::string error_msg = get_error_message( error );
 
-        std::cout << error_msg << std::endl;
+        destination << error_msg << std::endl;
     }
 
-    bool has_same_roots( const Solution& solution ) {
+    void output_two_roots( std::ostream& destination,
+                           const Solution& solution ) {
         Number x1 = get_first_root( solution );
         Number x2 = get_second_root( solution );
 
-        return is_equal( x1, x2 );
+        destination << FIRST_ROOT_MSG << to_string( x1 ) << std::endl;
+        destination << SECOND_ROOT_MSG << to_string( x2 ) << std::endl;
     }
 
-    void output_two_roots( const Solution& solution ) {
-        Number x1 = get_first_root( solution );
-        Number x2 = get_second_root( solution );
-
-        std::cout << FIRST_ROOT_MSG << to_string( x1 ) << std::endl;
-        std::cout << SECOND_ROOT_MSG << to_string( x2 ) << std::endl;
-    }
-
-    void output_single_root( const Solution& solution ) {
+    void output_single_root( std::ostream& destination,
+                             const Solution& solution ) {
         Number x = get_first_root( solution );
 
-        std::cout << SINGLE_ROOT_MSG << to_string( x ) << std::endl;
+        destination << SINGLE_ROOT_MSG << to_string( x ) << std::endl;
     }
 
-    void output_inf_roots() { std::cout << INF_ROOTS_MSG << std::endl; }
+    void output_inf_roots( std::ostream& destination ) {
+        destination << INF_ROOTS_MSG << std::endl;
+    }
 
-    void output_no_roots() { std::cout << NO_ROOTS_MSG << std::endl; }
+    void output_no_roots( std::ostream& destination ) {
+        destination << NO_ROOTS_MSG << std::endl;
+    }
 
-    void output_solution( const Solution& solution ) {
+    void output_solution( std::ostream& destination,
+                          const Solution& solution ) {
         SolutionType type = get_solution_type( solution );
 
-        if ( ( type == TWO_ROOTS ) && ( !has_same_roots( solution ) ) ) {
-            output_two_roots( solution );
-        }
-
-        else if ( ( type == TWO_ROOTS ) && ( has_same_roots( solution ) ) ) {
-            output_single_root( solution );
+        if ( type == TWO_ROOTS ) {
+            output_two_roots( destination, solution );
         }
 
         else if ( type == ONE_ROOT ) {
-            output_single_root( solution );
+            output_single_root( destination, solution );
         }
 
         else if ( type == INF_ROOTS ) {
-            output_inf_roots();
+            output_inf_roots( destination );
         }
 
         else {
-            output_no_roots();
+            output_no_roots( destination );
         }
     }
 }
 
-void output( const Solution& solution ) {
+void output( std::ostream& destination, const Solution& solution ) {
     if ( has_error( solution ) ) {
-        output_error_solution( solution );
+        output_error_solution( destination, solution );
         return;
     }
 
-    output_solution( solution );
+    output_solution( destination, solution );
 }
