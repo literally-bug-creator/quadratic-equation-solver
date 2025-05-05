5076253cbee4a4016d42ec7ab9950d0fdaddf9ff
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-05 12:36:36 +0300

    Fix `Number` impl

--- src/number/constants.cpp
+++ src/number/constants.cpp
@@ -2,17 +2,17 @@
 
 #include "error.hpp"
 
-namespace Messages {
+namespace NumberMessages {
     const std::string INVALID_INPUT = "Некорректный ввод";
     const std::string DIV_BY_ZERO = "Деление на ноль";
     const std::string ROOT_FROM_NEG = "Корень из отрицательного числа";
 }
 
-namespace Errors {
+namespace NumberErrors {
     const Error INVALID_INPUT =
-        make_error( ErrorCode::INVALID_INPUT, Messages::INVALID_INPUT );
+        make_error( ErrorCode::INVALID_INPUT, NumberMessages::INVALID_INPUT );
     const Error DIV_BY_ZERO =
-        make_error( ErrorCode::CALCULATION_ERROR, Messages::DIV_BY_ZERO );
-    const Error ROOT_FROM_NEG =
-        make_error( ErrorCode::CALCULATION_ERROR, Messages::ROOT_FROM_NEG );
+        make_error( ErrorCode::CALCULATION_ERROR, NumberMessages::DIV_BY_ZERO );
+    const Error ROOT_FROM_NEG = make_error( ErrorCode::CALCULATION_ERROR,
+                                            NumberMessages::ROOT_FROM_NEG );
 }
--- src/number/impl.cpp
+++ src/number/impl.cpp
@@ -1,33 +1,27 @@
-#include "number.hpp"
-
 #include <cmath>
 #include <optional>
 
+#include "constants.cpp"
 #include "input.hpp"
-#include "number_constants.cpp"
+#include "number.hpp"
 
 double get_value( const Number& coeff ) { return coeff.value; }
 
-Number
-make_number( const double value, const Error& error, const bool is_null ) {
-    return Number( value, error, is_null );
+Number make_number( const double value, const Error& error ) {
+    return Number( value, error );
 }
 
 Number input_number( std::istream& in ) {
     std::optional<double> number = read_value( in );
 
     if ( !number.has_value() ) {
-        return make_number( Numeric::ZERO, Errors::INVALID_INPUT, true );
+        return make_number( Numeric::ZERO, NumberErrors::INVALID_INPUT );
     }
 
     return make_number( number.value() );
 }
 
 Error get_error( const Number& number ) { return number.error; }
-
-bool is_null( const Number& coeff ) {
-    return coeff.is_null;
-} // Избыточный функционал у "Number"
 
 bool is_equal( const Number& left, const Number& right ) {
     return get_value( left ) == get_value( right );
@@ -62,7 +56,7 @@ Number div( const Number& dividend, const Number& divisor ) {
     double divisor_value = get_value( divisor );
 
     if ( divisor_value == Numeric::ZERO ) {
-        return make_number( Numeric::ZERO, Errors::DIV_BY_ZERO, true );
+        return make_number( Numeric::ZERO, NumberErrors::DIV_BY_ZERO );
     }
 
     return make_number( dividend_value / divisor_value );
@@ -72,7 +66,7 @@ Number sqrt( const Number& radicand ) {
     double value = get_value( radicand );
 
     if ( value < Numeric::ZERO ) {
-        return make_number( Numeric::ZERO, Errors::ROOT_FROM_NEG, true );
+        return make_number( Numeric::ZERO, NumberErrors::ROOT_FROM_NEG );
     }
 
     return make_number( sqrt( value ) );
