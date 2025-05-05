c487472e18a9a5854ee9d047257c7126bd19e35d
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-05 16:00:27 +0300

    Update `error` impl., implement `is_ok` func

--- src/error.cpp
+++ src/error.cpp
@@ -7,3 +7,8 @@ Error make_error( const ErrorCode code, const std::string& message ) {
 ErrorCode get_error_code( const Error& error ) { return error.code; }
 
 std::string get_error_message( const Error& error ) { return error.message; }
+
+bool is_ok( const Error& error ) {
+    ErrorCode code = get_error_code( error );
+    return code == ErrorCode::OK;
+}
