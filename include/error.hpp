207224ac86f2f8920e841c786b9cbbf26f81e12e
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-05 15:59:52 +0300

    Update `error` interface, add `is_ok` function

--- include/error.hpp
+++ include/error.hpp
@@ -22,3 +22,5 @@ Error make_error( const ErrorCode code, const std::string& message );
 ErrorCode get_error_code( const Error& error );
 
 std::string get_error_message( const Error& error );
+
+bool is_ok( const Error& error );
