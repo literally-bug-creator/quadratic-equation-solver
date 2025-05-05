c7875451ae6cce2c380fd6f27d20917933f1196a
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-05 12:26:17 +0300

    Update `solution` impl. structure

--- src/solution/impl.cpp
+++ src/solution/impl.cpp
@@ -1,34 +1,33 @@
-#include "solution.hpp"
-
+#include "constants.cpp"
+#include "equation_coefficients.hpp"
 #include "error.hpp"
 #include "number.hpp"
-#include "number_kit.hpp"
-#include "solution_constants.cpp"
+#include "solution.hpp"
 
-bool has_null( const NumberKit& nums ) {
-    Number a = get_a( nums );
-    Number b = get_b( nums );
-    Number c = get_c( nums );
+bool has_null( const EquationCoefficients& coeffs ) {
+    Number a = get_a( coeffs );
+    Number b = get_b( coeffs );
+    Number c = get_c( coeffs );
 
     return is_null( a ) || is_null( b ) || is_null( c );
 }
 
-bool has_error( const NumberKit& nums ) {
+bool has_error( const EquationCoefficients& nums ) {
     Error error = get_error( nums );
     ErrorCode code = get_error_code( error );
 
     return code != ErrorCode::OK;
 }
 
-bool is_valid( const NumberKit& nums ) {
+bool is_valid( const EquationCoefficients& nums ) {
     return !( has_error( nums ) || has_null( nums ) );
 }
 
-bool is_quadratic_equation( const NumberKit& nums ) {
+bool is_quadratic_equation( const EquationCoefficients& nums ) {
     return !is_equal( get_a( nums ), SolutionNumbers::ZERO );
 }
 
-Number get_discriminant( const NumberKit& nums ) {
+Number get_discriminant( const EquationCoefficients& nums ) {
     Number a = get_a( nums );
     Number b = get_b( nums );
     Number c = get_c( nums );
@@ -38,7 +37,7 @@ Number get_discriminant( const NumberKit& nums ) {
     return sub( square_b, semi_disc );
 }
 
-Number get_first_root( const NumberKit& nums ) {
+Number get_first_root( const EquationCoefficients& nums ) {
     Number neg_b = neg( get_b( nums ) );
     Number disc = get_discriminant( nums );
     Number disc_root = sqrt( disc );
@@ -47,7 +46,7 @@ Number get_first_root( const NumberKit& nums ) {
     return div( add( neg_b, disc_root ), two_a );
 }
 
-Number get_second_root( const NumberKit& nums ) {
+Number get_second_root( const EquationCoefficients& nums ) {
     Number a = get_a( nums );
     Number neg_b = neg( get_b( nums ) );
     Number disc = get_discriminant( nums );
@@ -57,7 +56,7 @@ Number get_second_root( const NumberKit& nums ) {
     return div( sub( neg_b, disc_root ), two_a );
 }
 
-bool has_no_roots( const NumberKit& nums ) {
+bool has_no_roots( const EquationCoefficients& nums ) {
     Number a = get_a( nums );
     Number b = get_b( nums );
     Number c = get_c( nums );
@@ -67,7 +66,7 @@ bool has_no_roots( const NumberKit& nums ) {
            !is_equal( c, SolutionNumbers::ZERO );
 }
 
-bool has_inf_roots( const NumberKit& nums ) {
+bool has_inf_roots( const EquationCoefficients& nums ) {
     Number a = get_a( nums );
     Number b = get_b( nums );
     Number c = get_c( nums );
@@ -77,7 +76,7 @@ bool has_inf_roots( const NumberKit& nums ) {
            is_equal( c, SolutionNumbers::ZERO );
 }
 
-Solution solve_quadratic_equation( const NumberKit& nums ) {
+Solution solve_quadratic_equation( const EquationCoefficients& nums ) {
     if ( is_lower_than( get_discriminant( nums ), SolutionNumbers::ZERO ) ) {
         return make_solution( NO_ROOTS,
                               SolutionNumbers::NUL,
@@ -96,14 +95,14 @@ Solution solve_quadratic_equation( const NumberKit& nums ) {
     return make_solution( SolutionType::TWO_ROOTS, x1, x2 );
 }
 
-Number get_single_root( const NumberKit& nums ) {
+Number get_single_root( const EquationCoefficients& nums ) {
     Number b = get_b( nums );
     Number neg_c = neg( get_c( nums ) );
 
     return div( neg_c, b );
 }
 
-Solution solve_linear_equation( const NumberKit& nums ) {
+Solution solve_linear_equation( const EquationCoefficients& nums ) {
     if ( has_no_roots( nums ) ) {
         return make_solution( SolutionType::NO_ROOTS,
                               SolutionNumbers::NUL,
@@ -128,7 +127,7 @@ Solution make_solution( const SolutionType& type,
     return Solution( type, first_root, second_root, error );
 }
 
-Solution get_solution( const NumberKit& nums ) {
+Solution get_solution( const EquationCoefficients& nums ) {
     if ( !is_valid( nums ) ) {
         return make_solution( SolutionType::NO_ROOTS,
                               SolutionNumbers::NUL,
