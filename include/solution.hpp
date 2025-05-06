13ed15e2a98c6d00936a92ec93ae9dcba1071e1b
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-06 17:36:48 +0300

    Add new `SolutionType`, solution's type API, refactor

--- include/solution.hpp
+++ include/solution.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "coefficients.hpp"
-#include "constants.hpp"
 #include "number.hpp"
 
-enum SolutionType { // TODO: Обдумать состояния
+enum SolutionType {
     TWO_ROOTS,
+    TWO_SAME_ROOTS,
     SINGLE_ROOT,
     NO_ROOTS,
     INF_ROOTS
@@ -24,13 +24,7 @@ struct Solution {
         type( type ), x1( x1 ), x2( x2 ), error( error ) {}
 };
 
-Solution make_solution(
-    const SolutionType& type,
-    const Number& first_root,
-    const Number& second_root,
-    const Error& error = Errors::OK );
-
-Solution solve_equation( const Coefficients& coeffs );
+const Error& get_error( const Solution& solution );
 
 const SolutionType& get_solution_type( const Solution& solution );
 
@@ -38,4 +32,14 @@ const Number& get_first_root( const Solution& solution );
 
 const Number& get_second_root( const Solution& solution );
 
-const Error& get_error( const Solution& solution );
+bool has_two_roots( const Solution& solution );
+
+bool has_two_same_roots( const Solution& solution );
+
+bool has_single_root( const Solution& solution );
+
+bool has_no_roots( const Solution& solution );
+
+bool has_inf_roots( const Solution& solution );
+
+Solution solve_equation( const Coefficients& coeffs );
