bdf931bd3360ce094234898d0d6939db0d3c376c
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-06 17:37:38 +0300

    Refactor, replace `SolutionType` check by `Solution` interface's functions (fix abst. leak)

--- src/output/impl.cpp
+++ src/output/impl.cpp
@@ -7,25 +7,28 @@
 #include "output.hpp"
 #include "solution.hpp"
 
-std::string get_unknown_solution_type_message( const Solution& solution ) {
+std::string get_unknown_solution_type_msg( const Solution& solution ) {
     return OutputMessages::UNKNOWN_SOLUTION_TYPE;
 }
 
-std::string get_inf_roots_solution_message( const Solution& solution ) {
-    return OutputMessages::INF_ROOTS;
+std::string get_no_roots_solution_msg( const Solution& solution ) {
+    return OutputMessages::NO_ROOTS;
 }
 
-std::string get_no_roots_solution_message( const Solution& solution ) {
-    return OutputMessages::NO_ROOTS;
+std::string get_inf_roots_solution_msg( const Solution& solution ) {
+    return OutputMessages::INF_ROOTS;
 }
 
 std::string get_single_root_message( const Number& root ) {
     return OutputMessages::SINGLE_ROOT + to_string( root );
 }
 
-std::string get_single_root_solution_message( const Solution& solution ) {
-    Number x = get_first_root( solution );
-    return get_single_root_message( x );
+std::string get_single_root_solution_msg( const Solution& solution ) {
+    return get_single_root_message( get_first_root( solution ) );
+}
+
+std::string get_two_same_roots_solution_msg( const Solution& solution ) {
+    return get_single_root_message( get_first_root( solution ) );
 }
 
 std::string get_two_roots_message( const Number& first_root,
@@ -36,14 +39,14 @@ std::string get_two_roots_message( const Number& first_root,
     return x1_msg + NEW_LINE + x2_msg;
 }
 
-std::string get_two_roots_solution_message( const Solution& solution ) {
+std::string get_two_roots_solution_msg( const Solution& solution ) {
     Number x1 = get_first_root( solution );
     Number x2 = get_second_root( solution );
 
     return get_two_roots_message( x1, x2 );
 }
 
-std::string get_error_message( const Solution& solution ) {
+std::string get_error_msg( const Solution& solution ) {
     Error error = get_error( solution );
     return get_error_message( error );
 }
@@ -53,25 +56,30 @@ bool has_error( const Solution& solution ) {
     return !is_ok( error );
 }
 
-std::string get_solution_message( const Solution& solution ) {
-    if ( has_error( solution ) ) { return get_error_message( solution ); }
+std::string get_solution_msg( const Solution& solution ) {
+    if ( has_error( solution ) ) { return get_error_msg( solution ); }
 
-    switch ( get_solution_type( solution ) ) {
-    case SolutionType::TWO_ROOTS:
-        return get_two_roots_solution_message( solution );
-
-    case SolutionType::SINGLE_ROOT:
-        return get_single_root_solution_message( solution );
-
-    case SolutionType::NO_ROOTS:
-        return get_no_roots_solution_message( solution );
-
-    case SolutionType::INF_ROOTS:
-        return get_inf_roots_solution_message( solution );
-
-    default:
-        return get_unknown_solution_type_message( solution );
+    if ( has_two_roots( solution ) ) {
+        return get_two_roots_solution_msg( solution );
     }
+
+    else if ( has_two_same_roots( solution ) ) {
+        return get_two_same_roots_solution_msg( solution );
+    }
+
+    else if ( has_single_root( solution ) ) {
+        return get_single_root_solution_msg( solution );
+    }
+
+    else if ( has_inf_roots( solution ) ) {
+        return get_inf_roots_solution_msg( solution );
+    }
+
+    else if ( has_no_roots( solution ) ) {
+        return get_no_roots_solution_msg( solution );
+    }
+
+    return get_unknown_solution_type_msg( solution );
 }
 
 void output_string( const std::string& str ) {
@@ -79,6 +87,6 @@ void output_string( const std::string& str ) {
 }
 
 void output( const Solution& solution ) {
-    std::string message = get_solution_message( solution );
-    output_string( message );
+    std::string msg = get_solution_msg( solution );
+    output_string( msg );
 }
