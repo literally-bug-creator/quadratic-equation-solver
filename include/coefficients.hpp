b0ee62c649d91375f75a52dad58d54b8109271c0
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-10 14:28:43 +0300

    Remove comments

--- include/coefficients.hpp
+++ include/coefficients.hpp
@@ -17,10 +17,10 @@ struct Coefficients {
 
 Coefficients input_coefficients();
 
-const Number& get_a( const Coefficients& nums );
+const Number& get_a( const Coefficients& coeffs );
 
-const Number& get_b( const Coefficients& nums );
+const Number& get_b( const Coefficients& coeffs );
 
-const Number& get_c( const Coefficients& nums );
+const Number& get_c( const Coefficients& coeffs );
 
-const Error& get_error( const Coefficients& nums );
+const Error& get_error( const Coefficients& coeffs );
