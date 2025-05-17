e0ccb33468cdf0478ce4ba0d945fdbf8bf769ef1
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-17 10:36:44 +0300

    Change `struct Solution` by `pair`

--- include/solution.hpp
+++ include/solution.hpp
@@ -9,31 +9,21 @@ enum SolutionType {
     INF_ROOTS
 };
 
-struct Solution {
-    const SolutionType type;
-    const Number x1;
-    const Number x2;
-    const Error error;
-
-    Solution( const SolutionType& type,
-              const Number& x1,
-              const Number& x2,
-              const Error& error ):
-        type( type ), x1( x1 ), x2( x2 ), error( error ) {}
-};
+using Solution =
+    std::pair<std::pair<SolutionType, Number>, std::pair<Number, Error>>;
 
 const Solution make_solution( const SolutionType& type,
                               const Number& first_root,
                               const Number& second_root,
                               const Error& error = Errors::OK );
 
-const Error& get_error( const Solution& solution );
+const Error get_error( const Solution& solution );
 
-const SolutionType& get_solution_type( const Solution& solution );
+SolutionType get_solution_type( const Solution& solution );
 
-const Number& get_first_root( const Solution& solution );
+const Number get_first_root( const Solution& solution );
 
-const Number& get_second_root( const Solution& solution );
+const Number get_second_root( const Solution& solution );
 
 bool has_two_roots( const Solution& solution );
 
--- src/solution.cpp
+++ src/solution.cpp
@@ -1,26 +1,49 @@
 #include "solution.hpp"
+#include <any>
 
 #include "error.hpp"
 #include "number.hpp"
+#include "selector.hpp"
+
+Selector get_selector( const Solution& solution ) {
+    std::function<std::any( int index )> field =
+        [solution]( int index ) -> std::any {
+        if ( index == 0 ) {
+            return std::any( solution.first.first );
+        } else if ( index == 1 ) {
+            return std::any( solution.first.second );
+        } else if ( index == 2 ) {
+            return std::any( solution.second.first );
+        } else {
+            return std::any( solution.second.second );
+        }
+    };
+
+    return field;
+}
 
 const Solution make_solution( const SolutionType& type,
                               const Number& first_root,
                               const Number& second_root,
                               const Error& error ) {
-    return Solution( type, first_root, second_root, error );
+    return Solution( { type, first_root }, { second_root, error } );
 }
 
-const SolutionType& get_solution_type( const Solution& solution ) {
-    return solution.type;
+SolutionType get_solution_type( const Solution& solution ) {
+    return std::any_cast<SolutionType>(get_selector(solution)(0));
 }
 
-const Number& get_first_root( const Solution& solution ) { return solution.x1; }
-
-const Number& get_second_root( const Solution& solution ) {
-    return solution.x2;
+const Number get_first_root( const Solution& solution ) {
+    return std::any_cast<Number>(get_selector(solution)(1));
 }
 
-const Error& get_error( const Solution& solution ) { return solution.error; }
+const Number get_second_root( const Solution& solution ) {
+    return std::any_cast<Number>(get_selector(solution)(2));
+}
+
+const Error get_error( const Solution& solution ) {
+    return std::any_cast<Error>(get_selector(solution)(3));
+}
 
 bool is_equal( const SolutionType a, const SolutionType b ) { return a == b; }
 
