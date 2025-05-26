e201814f6c52af295b4e46d4fba5d2a5001564a2
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-27 00:03:54 +0300

    Refactor `Output`

--- src/output/impl.cpp
+++ src/output/impl.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <string>
 
@@ -7,43 +8,58 @@
 #include "output.hpp"
 #include "solution.hpp"
 
-std::string get_unknown_solution_type_msg( const Solution& solution ) {
-    return OutputMessages::UNKNOWN_SOLUTION_TYPE;
+std::string build_solution_type_msg( const Solution& solution ) {
+    switch ( get_solution_type( solution ) ) {
+    case SolutionType::INF_ROOTS:
+        return OutputMessages::INF_ROOTS;
+
+    case SolutionType::NO_ROOTS:
+        return OutputMessages::NO_ROOTS;
+
+    default:
+        return EMPTY_STR;
+    }
 }
 
-std::string get_no_roots_solution_msg( const Solution& solution ) {
-    return OutputMessages::NO_ROOTS;
+bool is_roots_equals( std::vector<Number> roots ) {
+    if ( roots.empty() ) return false;
+    if ( roots.size() == ONE ) return true;
+
+    for ( size_t index = ONE; index < roots.size(); index++ ) {
+        if ( !is_equal( roots[index], roots[index - ONE] ) ) return false;
+    }
+
+    return true;
 }
 
-std::string get_inf_roots_solution_msg( const Solution& solution ) {
-    return OutputMessages::INF_ROOTS;
+std::string get_no_index_root_msg( const Number& root ) {
+    return X_STR + EQUAL_SIGN_STR + to_string( root );
 }
 
-std::string get_single_root_message( const Number& root ) {
-    return OutputMessages::SINGLE_ROOT + to_string( root );
+std::string get_index_root_msg( const Number& root, size_t index ) {
+    return X_STR + std::to_string( index ) + EQUAL_SIGN_STR + to_string( root );
 }
 
-std::string get_single_root_solution_msg( const Solution& solution ) {
-    return get_single_root_message( get_first_root( solution ) );
+std::string build_root_msg( const Number& root,
+                            size_t index = OUTBOUND_INDEX ) {
+    if ( index == OUTBOUND_INDEX ) return get_no_index_root_msg( root );
+    return get_index_root_msg( root, index );
 }
 
-std::string get_two_same_roots_solution_msg( const Solution& solution ) {
-    return get_single_root_message( get_first_root( solution ) );
-}
+std::string build_solution_roots_msg( const Solution& solution ) {
+    std::vector<Number> roots = get_roots( solution );
 
-std::string get_two_roots_message( const Number& first_root,
-                                   const Number& second_root ) {
-    std::string x1_msg = OutputMessages::FIRST_ROOT + to_string( first_root );
-    std::string x2_msg = OutputMessages::SECOND_ROOT + to_string( second_root );
+    if ( is_roots_equals( roots ) ) return build_root_msg( roots[ZERO] );
 
-    return x1_msg + NEW_LINE + x2_msg;
-}
+    std::string msg = EMPTY_STR;
 
-std::string get_two_roots_solution_msg( const Solution& solution ) {
-    Number x1 = get_first_root( solution );
-    Number x2 = get_second_root( solution );
+    for ( size_t index = ZERO; index < roots.size(); index++ ) {
+        msg += build_root_msg( roots[index], index + ONE );
 
-    return get_two_roots_message( x1, x2 );
+        if ( index < roots.size() - ONE ) msg += NEW_LINE;
+    }
+
+    return msg;
 }
 
 std::string get_error_msg( const Solution& solution ) {
@@ -56,25 +72,19 @@ bool has_error( const Solution& solution ) {
     return !is_ok( error );
 }
 
-std::string get_solution_msg( const Solution& solution ) {
+std::string build_solution_msg( const std::string& type_msg,
+                                const std::string& roots_msg ) {
+    if ( type_msg.empty() ) return roots_msg;
+    return type_msg;
+}
+
+std::string build_solution_msg( const Solution& solution ) {
     if ( has_error( solution ) ) { return get_error_msg( solution ); }
 
-    if ( has_two_roots( solution ) )
-        return get_two_roots_solution_msg( solution );
+    std::string type_msg = build_solution_type_msg( solution );
+    std::string roots_msg = build_solution_roots_msg( solution );
 
-    else if ( has_two_same_roots( solution ) )
-        return get_two_same_roots_solution_msg( solution );
-
-    else if ( has_single_root( solution ) )
-        return get_single_root_solution_msg( solution );
-
-    else if ( has_inf_roots( solution ) )
-        return get_inf_roots_solution_msg( solution );
-
-    else if ( has_no_roots( solution ) )
-        return get_no_roots_solution_msg( solution );
-
-    return get_unknown_solution_type_msg( solution );
+    return build_solution_msg( type_msg, roots_msg );
 }
 
 void output_string( const std::string& str ) {
@@ -82,6 +92,6 @@ void output_string( const std::string& str ) {
 }
 
 void output( const Solution& solution ) {
-    std::string msg = get_solution_msg( solution );
+    std::string msg = build_solution_msg( solution );
     output_string( msg );
 }
