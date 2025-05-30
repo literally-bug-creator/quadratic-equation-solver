bde7cbc5d2fd00e164489774000e72dc9bbd7e6b
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-30 18:18:55 +0300

    Add `Interpreted` feature's part - `Context`

---
+++ include/context.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstddef>
+#include <vector>
+
+#include "token_sequence.hpp"
+
+class Context {
+private:
+    TokenSequenceOwner tokens;
+    size_t index = 0;
+    std::vector<size_t> history;
+
+public:
+    Context( std::vector<Token> tokens );
+
+    bool is_finished() const;
+    const TokenSequenceView get_tokens( size_t index, size_t length ) const;
+    void next( size_t length );
+    void dump();
+    void rollback();
+};
---
+++ src/context.cpp
@@ -0,0 +1,25 @@
+#include "context.hpp"
+
+#include <cstddef>
+#include <stdexcept>
+#include <vector>
+
+#include "token_sequence.hpp"
+
+Context::Context( std::vector<Token> tokens ): tokens( tokens ) {};
+
+bool Context::is_finished() const { return index >= tokens.size(); }
+
+const TokenSequenceView Context::get_tokens( size_t index,
+                                             size_t length ) const {
+    return make_view( tokens, index, length );
+}
+
+void Context::next( size_t length ) { index += length; }
+
+void Context::dump() { history.push_back( index ); }
+
+void Context::rollback() {
+    if ( history.empty() ) throw std::runtime_error( "" );
+    index = history.back();
+}
