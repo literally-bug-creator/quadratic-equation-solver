0c9f95c03788394efe8305c44fadac203b2c8d28
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-04 15:06:46 +0300

    Restructure project - move `*.hpp` & `*.cpp` to `quadratic-equation-solver` folder

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "coefficients.hpp"
-#include "output.hpp"
-#include "solver.hpp"
-
-int main() {
-    Coefficients coeffs = input_coefficients();
-    Solution solution = solve_equation( coeffs );
-    output( solution );
-}
